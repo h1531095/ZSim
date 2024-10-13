815ceaa7f58f9fefac031f69cb7d8171c5dbdf6b
TommrraraSnow <tommrrara@foxmail.com>
2024-10-14 01:01:21 +0800

    Bug Fixed

--- Report.py
+++ Report.py
@@ -7,12 +7,12 @@ def report_to_log(content):
     # 获取当前日期和时间
     if json.load(open('config.json')).get('debug'):
         now = datetime.now()
-        timestamp = now.strftime(r'%Y-%m-%d_%H:%M:%S')
+        timestamp = now.strftime(r'%Y-%m-%d_%H%M%S')
         report_file_path = f'./logs/{timestamp}.log'
 
         # 确保目录存在
         os.makedirs(os.path.dirname(report_file_path), exist_ok=True)
 
         # 写入日志
-        with open(report_file_path, 'a') as file:
+        with open(report_file_path, 'a', encoding='utf-8') as file:
             file.write(content + '\n')
