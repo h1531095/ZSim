dc325c87721c3a0155de27b5412d4bc11312cf3f
TommrraraSnow <tommrrara@foxmail.com>
2024-10-15 07:14:48 +0800

    代码优化和功能调整
    - 更新了.Calcultor.py中的方法定义，增加了staticmethod修饰符
    - 修正了.CharSet_new.py中的参数类型检查逻辑
    - 修改了.Skill_Class.py中的技能信息获取逻辑
    - 更新了.gitignore文件，添加了新的忽略项
    - 调整了config.json，关闭了debug模式

--- Calcultor.py
+++ Calcultor.py
@@ -3,7 +3,7 @@ from EnemyClass import Enemy
 from define import *
 
 
-# from BuffExsist_Judge import buff_exsist_judge
+# from BuffExist_Judge import buff_exist_judge
 
 class Caculator:
     def __init__(self, char_statement: Character, enemy_statement: Enemy):
@@ -11,23 +11,33 @@ class Caculator:
         self.char_level = char_statement.level
         self.enemy_statement = enemy_statement
 
-    def cal_dmg_expect(self, char_statement, enemy_statement):
+    @staticmethod
+    def cal_dmg_expect(char_statement, enemy_statement):
         dmg_expect = 0
         return dmg_expect
 
-    def cal_dmg_crit(self, char_statement, enemy_statement):
+    @staticmethod
+    def cal_dmg_crit(char_statement, enemy_statement):
         dmg_crit = 0
         return dmg_crit
 
-    def cal_dmg_not_crit(self, char_statement, enemy_statement):
+    @staticmethod
+    def cal_dmg_not_crit(char_statement, enemy_statement):
         dmg_not_crit = 0
         return dmg_not_crit
 
-    def cal_anomaly_dmg(self, char_statement, enemy_statement):
-        anomaly_dmg_expect = 0
-        return anomaly_dmg_expect
+    @staticmethod
+    def cal_anomaly_dmg(char_statement, enemy_statement):
+        anomaly_dmg = 0
+        return anomaly_dmg
 
-    def cal_anomaly_buildup(self, char_statement, enemy_statement):
+    @staticmethod
+    def cal_anomaly_dmg_equally():
+        anomaly_dmg_equally = 0
+        return anomaly_dmg_equally
+
+    @staticmethod
+    def cal_anomaly_buildup(char_statement, enemy_statement):
         anomaly_buildup_expect = 0
         return anomaly_buildup_expect
 
