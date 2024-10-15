085fd5b196e6df155b0dbcd52cd4d44bfa3780cd
Steinwaysj <1012399286@qq.com>
2024-10-16 06:56:15 +0800

    update 0.1.2 20241016_1
    更新了EventClass，

--- EventClass.py
+++ EventClass.py
@@ -1,4 +1,5 @@
-from Skill_Class import Skill
+from TickClass import TIMETICK
+import math
 
 
 class Event:
@@ -7,25 +8,31 @@ class Event:
     以后可能的拓展方向：后台协同攻击
 
     """
-    def __init__(self):
-        self.ft = self.EventFeature()
-        self.dy = self.EventDynamic()
-
-    class EventFeature:
-        """
-        记录了该事件的一些基本信息，比如事件的持续时间，事件的判定生效时间
-        """
-        def __init__(self):
-            self.duration = 0
-            self.judging_duration = 0
+    def __init__(self, start_ticks, end_ticks, settelment_count):
+        self.dy = self.EventDynamic(start_ticks, end_ticks, settelment_count)
 
     class EventDynamic:
-        def __init__(self, timenow):
-            self.startticks = 0
-            self.endticks = 0
+        def __init__(self, start_ticks, end_ticks, settelment_count):
+            self.start_ticks = start_ticks
+            self.end_ticks = end_ticks
             self.settlement_ticks = []  # 事件的结算节点，对于Skill来说，就是hit发生的ticks
-            self.is_happening = False
+            step = (end_ticks - start_ticks) / settelment_count
+            for i in range(settelment_count):
+                self.settlement_ticks.append(start_ticks + (i + 1) * step)
 
+    def is_happening(self):
+        if TIMETICK in range(self.dy.start_ticks, self.dy.end_ticks):
+            return True
+        else:
+            return False
+
+    def check_current_event(self):
+        if TIMETICK == self.dy.start_ticks:
+            return "start"
+        elif TIMETICK == self.dy.end_ticks:
+            return "end"
+        elif TIMETICK == math.ceil(self.dy.settlement_ticks[0]):
+            return "settlement"
 
 
 
