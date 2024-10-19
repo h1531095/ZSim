dc4ef9d4faadda5245cc6588ca7f286ea539bee6
Steinwaysj <1012399286@qq.com>
2024-10-20 06:03:45 +0800

    update 0.1.2 20241020_1
    更新了SkillEventSplit函数在面对单线程技能序列时的各项功能，包括：主任务启动识别，子任务自动拆分，子任务完成及删除。

--- SkillEventSplit.py
+++ SkillEventSplit.py
@@ -3,53 +3,77 @@ from Skill_Class import Skill
 import Preload
 from TickClass import Tick
 import pandas as pd
+import math
 
 
-def SkillEventSplit(skills_queue: LinkedList, time_now: float):
-    dynamic_loading_dict = {}
+def SkillEventSplit(skills_queue: LinkedList):
+    Load_mission_dict = {}
     for skill in skills_queue:
         if not isinstance(skill, Preload.SkillNode):
             raise ValueError(f"本次拆分的{skill}不是SkillNode类！")
-        if skill.preload_tick <= time_now:
-            loadmission = LoadingMission(skill)
-            loadmission.mission_start(skill)
-            dynamic_loading_dict[skill.skill_tag] = loadmission
-            print(f"已经对{skill.skill_tag}进行加载，执行列表为{dynamic_loading_dict[skill.skill_tag].mission_dict}")
-    return dynamic_loading_dict
+        Load_mission_dict[skill.skill_tag] = LoadingMission(skill)
+    return Load_mission_dict
+
+
+def StartJudge(Load_mission_dict: dict, time_now: float):
+    for mission in Load_mission_dict:
+        if not isinstance(Load_mission_dict[mission], LoadingMission):
+            raise TypeError(f'本次检验的{mission}并非LoadingMission类！')
+        if Load_mission_dict[mission].mission_already_start:
+            continue
+            # 如果任务显示为已经被加载过，那么就跳过加载。这样可以避免重复加载。
+        if Load_mission_dict[mission].mission_start_tick <= time_now:
+            Load_mission_dict[mission].mission_start()
+            print(f"当前tick{time_now}，{Load_mission_dict[mission].mission_tag}任务开始了，子任务列表为：{Load_mission_dict[mission].mission_dict}")
+
+
+def SubMissionLoop(Load_mission_dict: dict, time_now: float):
+    for mission in Load_mission_dict:
+        if not isinstance(Load_mission_dict[mission], LoadingMission):
+            raise TypeError(f"正在加载的{mission}并非LoadingMission类！！")
+        if not Load_mission_dict[mission].mission_active_state:
+            continue
+        for sub_mission_tick in list(Load_mission_dict[mission].mission_dict):
+            if sub_mission_tick > time_now:
+                continue
+            if Load_mission_dict[mission].mission_dict[sub_mission_tick] == 'start':
+                del Load_mission_dict[mission].mission_dict[sub_mission_tick]
+                print(f'当前tick{time_now}，{mission}任务的子任务序启动！start事件已经被移除，当前子任务列表为：{Load_mission_dict[mission].mission_dict}')
+            else:
+                if not Load_mission_dict[mission].mission_dict[sub_mission_tick] == 'end':
+                    print(f'当前tick{time_now}，{mission}事件的子任务{Load_mission_dict[mission].mission_dict[sub_mission_tick]}已经执行')
+                    del Load_mission_dict[mission].mission_dict[sub_mission_tick]
+                    print(f'剩余子任务列表为：{Load_mission_dict[mission].mission_dict}')
+                    continue
+                Load_mission_dict[mission].mission_end()
+                print(f'当前tick{time_now}，{mission}事件的全部子任务都已经完成！剩余子任务列表为：{Load_mission_dict[mission].mission_dict}')
 
 
 class LoadingMission:
     def __init__(self, skill: Preload.SkillNode):
         self.mission_tag = skill.skill_tag
         self.mission_dict = {}
+        self.mission_already_start = False
         self.mission_active_state = False
         self.mission_start_tick = skill.preload_tick
+        self.skill = skill
 
-    def mission_start(self, skill: Preload.SkillNode):
+    def mission_start(self):
         self.mission_active_state = True
-        timecost = skill.skill.ticks
-        timestep = (timecost - 1)/skill.hit_times
-        self.mission_dict[skill.preload_tick] = "start"
-        for i in range(skill.hit_times):
-            tick_key = skill.preload_tick + timestep * (i+1)
+        self.mission_already_start = True
+        timecost = self.skill.skill.ticks
+        time_step = (timecost - 1)/self.skill.hit_times
+        self.mission_dict[float(self.skill.preload_tick)] = "start"
+        for i in range(self.skill.hit_times):
+            tick_key = float(math.ceil(self.skill.preload_tick + time_step * (i+1)))
+            # 由于timetick在循环中的自增量是整数，所以为了保证能和键值准确匹配，
+            # 这里的键值也要向上取整，注意，这里产生的是一个int，所以要转化为float
             self.mission_dict[tick_key] = "hit"
-        self.mission_dict[skill.preload_tick + timecost] = "end"
+        self.mission_dict[self.skill.preload_tick + timecost] = "end"
 
-    def mission_end(self, dynamic_mission_dict: dict):
+    def mission_end(self):
         self.mission_active_state = False
         self.mission_dict = {}
-        dynamic_mission_dict.pop(self.mission_tag)
-
-    def sub_mission_completed(self, ticks: float):
-        if ticks not in self.mission_dict:
-            raise ValueError(f"{ticks}并不是当前子任务的时间节点！")
-        print(f"完成子任务{self.mission_dict[ticks]}，当前子任务属于{self.mission_tag}")
-        self.mission_dict.pop(ticks)
-
-    def sub_mission_check(self, ticknow: float):
-        for items in self.mission_dict[:]:
-            if items <= ticknow:
-                self.sub_mission_completed(ticknow)
 
 
 if __name__ == "__main__":      # 测试
@@ -57,16 +81,15 @@ if __name__ == "__main__":      # 测试
     timestep = 1
     timelimit = 300
     timenow = timestart
-    Dynamic_loading_dict = {}
     test = {
         'skill_tag': ['1221_NA_1', '1221_NA_2', '1221_NA_3', '1221_NA_4', '1221_NA_5']
     }
     test_skill_dataframe = pd.DataFrame(test)
     test_object = Skill(CID=1221)
     skill_queue = Preload.SkillsQueue.get_skills_queue(test_skill_dataframe, test_object)
-    while timenow < timelimit:
-        Dynamic_loading_dict = SkillEventSplit(skill_queue, timenow)
+    load_mission_dict = SkillEventSplit(skill_queue)
+    while timenow <= timelimit:
+        StartJudge(load_mission_dict, timenow)
+        SubMissionLoop(load_mission_dict,  timenow)
         timenow += timestep
-    for item in Dynamic_loading_dict[:]:
-        print(f"当前tick是{Dynamic_loading_dict[item].mission_start_tick}，当前任务dict属于{item}，具体执行列表为：{Dynamic_loading_dict[item].mission_dict}")
 
