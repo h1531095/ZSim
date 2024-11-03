ba1ea9ef38276bdf9eab880499e50b21eb8c771a
Steinwaysj <1012399286@qq.com>
2024-11-04 04:11:10 +0800

    删除外面的Buff系列文件
    删除BuffClass、BuffLoad、BuffExist_Judge、BuffAdd、以及一个弃用的老文件BonusCalculate

--- BonusCalcualte.py
+++
@@ -1,7 +0,0 @@
-def BonusCal(basic, bonus_list):
-    if len(bonus_list) != 2:
-        raise ValueError('bonus_list必须包含2个元素！')
-    fix = bonus_list[0]
-    bonus = bonus_list[1]
-    result = basic * (1 + bonus) + fix
-    return result
