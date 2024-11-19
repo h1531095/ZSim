2d7d44fb21d105d5a3cbbc5ce92309bc42c42a53
TommrraraSnow <tommrrara@foxmail.com>
2024-11-19 09:05:28 +0800

    feat(RNG): 增加基于时间的随机数生成
    - 引入 sys 模块以获取主模块的 tick 属性- 修改 __init__ 方法，结合时间戳和 tick 值生成种子
    - 优化了无参调用时的随机数生成逻辑，提高随机性

--- RandomNumberGenerator/__init__.py
+++ RandomNumberGenerator/__init__.py
@@ -1,14 +1,20 @@
 import random
 import time
 from functools import lru_cache
+import sys
 
 MAX_SIGNED_INT64 = 2 ** 63 - 1
 
 
 class RNG:
     def __init__(self, seed: int = None):
-        seed = seed or int(time.time())
-        self.seed, self.r = self.generate_random_number(seed)
+        main_module = sys.modules['__main__']
+        tick: int = getattr(main_module, 'tick', 0)
+        if seed is None:
+            seed = int(time.time() * 1000000) + tick
+        else:
+            seed = int(seed) + tick
+        (self.seed, self.r) = self.generate_random_number(seed)
 
     @staticmethod
     @lru_cache(maxsize=4)
