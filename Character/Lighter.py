c6e9e632c1f09fa42cfc8eb1fe224f3fa7bc9f33
TommrraraSnow <tommrrara@foxmail.com>
2024-11-20 11:06:30 +0800

    refactor(Character): 重构莱特的士气计算逻辑
    -调整士气恢复和消耗的计算方式，确保逻辑更清晰
    - 优化代码结构，提高可读性和可维护性
    - 移除不必要的条件判断，简化逻辑流程

--- Character/Lighter.py
+++ Character/Lighter.py
@@ -42,23 +42,12 @@ class Lighter(Character):
         for node in skill_nodes:
             # 累加逻辑
             if self.morale < 10000:
-                main_module = sys.modules['__main__']
-                tick: int = main_module.tick
-                # 每 6 ticks 更新
-                if (minus := tick - self.last_tick) >= 6:
-                    self.morale += minus // 6 * 29     # 地板除保证整形对齐
-                    self.last_tick = tick - minus % 6   # 求余以保证余数不计入本次计算
                 # 消耗能量及时更新
                 sp_consume = node.skill.sp_consume
-                if sp_consume <= 0:
-                    continue
-                else:
+                if sp_consume > 0:
                     self.morale += sp_consume * 26
-                self.morale = min(self.morale, 10000)
-
             if '1161' not in node.skill_tag:
                 continue
-
             # 递减逻辑
             if node.skill_tag == '1161_NA_5_SH_EX':
                 self.morale -= 1000
@@ -70,3 +59,13 @@ class Lighter(Character):
             if self.morale < 0:
                 report_to_log(f"[Character] 莱特的士气消耗至 {self.morale / 100:.2f}, 请检查")
                 self.morale = 0
+
+        # 时间每 6 ticks 更新
+        main_module = sys.modules['__main__']
+        tick: int = main_module.tick
+        if (minus := tick - self.last_tick) >= 6:
+            self.morale += minus // 6 * 29     # 地板除保证整形对齐
+            self.last_tick = tick - minus % 6   # 求余以保证余数不计入本次计算
+        self.morale = min(self.morale, 10000)
+        morale = self.morale
+        pass
